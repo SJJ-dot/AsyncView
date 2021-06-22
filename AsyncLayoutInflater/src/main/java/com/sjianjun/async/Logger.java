67581426bce6f7d602b22ae041fa5464aec3eb1f
SJJ <978622005@qq.com>
2021-06-22 09:53:09 +0800

    Logger empty

--- AsyncLayoutInflater/src/main/java/com/sjianjun/async/Logger.java
+++ AsyncLayoutInflater/src/main/java/com/sjianjun/async/Logger.java
@@ -2,4 +2,11 @@ package com.sjianjun.async;
 
 public interface Logger {
     void log(String msg,Throwable e);
+
+    Logger empty = new Logger() {
+        @Override
+        public void log(String msg, Throwable e) {
+
+        }
+    };
 }
